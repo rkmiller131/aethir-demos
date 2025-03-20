8856fad33a58a34eb78138241ae2ec83bd8585ce
Rachel Miller <rkmiller131@gmail.com>
2025-03-20 11:09:05 -0700

    Create page.tsx

---
+++ app/landing/page.tsx
@@ -0,0 +1,12 @@
+export default function Landing() {
+  return (
+    <div className="w-[100dvw] h-[100dvh]">
+      <video
+        src=""
+        className="object-fit"
+      >
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+}
