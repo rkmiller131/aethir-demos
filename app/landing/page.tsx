e2a87ea40c8c58d68a56d083817d4662f37434d4
Rachel Miller <rkmiller131@gmail.com>
2025-03-20 11:10:58 -0700

    Update page.tsx

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -2,9 +2,14 @@ export default function Landing() {
   return (
     <div className="w-[100dvw] h-[100dvh]">
       <video
-        src=""
-        className="object-fit"
+        autoPlay
+        muted
+        loop
+        playsInline
+        preload="auto"
+        className="h-full w-full object-cover z-[-1]"
       >
+        <source src="https://cdn.glitch.me/c4f540ac-7f7c-41b2-ae89-9e2617351aa6/banner-trailer.webm?v=1742318229191" type="video/webm" />
         Your browser does not support the video tag.
       </video>
     </div>
