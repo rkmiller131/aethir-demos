6415a853f0ac92cafc8ddf04b0870ebb774c25b3
Nathan Potter <71411986+nathanpotter17@users.noreply.github.com>
2025-03-26 17:41:24 -0400

    feat: clean up

--- app/gamepass/(components)/UI/Carousel.tsx
+++ app/gamepass/(components)/UI/Carousel.tsx
@@ -4,10 +4,21 @@ import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import games from '../../../lib/games.json';
+import { startGame } from '@/app/actions';
+import { APP_PAGES } from '@/app/lib/constants';
+import { AppPageType } from '@/app/lib/types';
 
 export default function Carousel() {
   const [startIndex, setStartIndex] = useState(0);
   const [visibleCount, setVisibleCount] = useState(1);
+  const [isStarting, setIsStarting] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 700);
+    return () => clearTimeout(timer);
+  }, []);
 
   useEffect(() => {
     const updateVisibleCount = () => {
@@ -18,7 +29,9 @@ export default function Carousel() {
 
     updateVisibleCount();
     window.addEventListener('resize', updateVisibleCount);
-    return () => window.removeEventListener('resize', updateVisibleCount);
+    return () => {
+      window.removeEventListener('resize', updateVisibleCount);
+    };
   }, []);
 
   const scroll = (direction: 'left' | 'right') => {
@@ -29,11 +42,24 @@ export default function Carousel() {
     );
   };
 
+  const handlePlayNow = async () => {
+    if (isStarting) return;
+
+    try {
+      setIsStarting(true);
+      await startGame(APP_PAGES.GAMEPASS as AppPageType);
+    } catch (error) {
+      console.error('Error starting game:', error);
+    } finally {
+      setIsStarting(false);
+    }
+  };
+
   return (
     <div className="flex flex-col max-w-[1232px] mx-auto pb-4">
       <div className="flex justify-between items-center mb-4">
         <div>
-          <h1 className="text-2xl font-bold">Picks for you</h1>
+          <h1 className="text-2xl font-bold mb-2">Picks for you</h1>
           <p className="text-gray-500/75">
             With games added all the time, you&apos;ll always have something new
             to play.
@@ -65,26 +91,51 @@ export default function Carousel() {
         </div>
       </div>
       <div className="flex gap-4 overflow-hidden">
-        {games.slice(0, 6).map((item, i) => (
-          <div
-            key={i}
-            className="flex-shrink-0 w-48 h-[350px] bg-gray-800 rounded-lg overflow-hidden flex flex-col hover:brightness-75 transition"
-          >
-            <div className="relative flex-grow">
-              <Image
-                src={item.portrait}
-                alt={item.name}
-                layout="fill"
-                objectFit="cover"
-                className="rounded-t-lg"
-              />
-            </div>
-            <div className="p-3">
-              <p className="text-white font-bold truncate">{item.name}</p>
-              <p className="text-gray-300 text-sm truncate">{item.pub}</p>
-            </div>
-          </div>
-        ))}
+        {loading
+          ? Array(6)
+              .fill(0)
+              .map((_, i) => (
+                <div
+                  key={i}
+                  className={
+                    i === 0
+                      ? 'flex-shrink-0 w-48 h-[350px] bg-gray-800/50 rounded-lg overflow-hidden flex flex-col cursor-pointer hover:brightness-75 transition animate-[pulse_0.7s_infinite]'
+                      : 'flex-shrink-0 w-48 h-[350px] bg-gray-800/50 rounded-lg overflow-hidden cursor-not-allowed flex flex-col hover:brightness-75 transition animate-[pulse_0.7s_infinite]'
+                  }
+                >
+                  <div className="relative flex-grow"></div>
+                  <div className="p-3"></div>
+                </div>
+              ))
+          : games.slice(0, 6).map((item, i) => (
+              <div
+                key={i}
+                className={
+                  i === 0
+                    ? 'flex-shrink-0 w-48 h-[350px] bg-gray-800/50 rounded-lg overflow-hidden flex flex-col cursor-pointer hover:brightness-75 transition'
+                    : 'flex-shrink-0 w-48 h-[350px] bg-gray-800/50 rounded-lg overflow-hidden cursor-not-allowed flex flex-col hover:brightness-75 transition'
+                }
+                onClick={() => {
+                  if (i === 0) {
+                    handlePlayNow();
+                  }
+                }}
+              >
+                <div className="relative flex-grow">
+                  <Image
+                    src={item.portrait}
+                    alt={item.name}
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-t-lg"
+                  />
+                </div>
+                <div className="p-3">
+                  <p className="text-white font-bold truncate">{item.name}</p>
+                  <p className="text-gray-300 text-sm truncate">{item.pub}</p>
+                </div>
+              </div>
+            ))}
       </div>
     </div>
   );
--- app/gamepass/(components)/UI/Navbar.tsx
+++ app/gamepass/(components)/UI/Navbar.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import games from '../../../lib/games.json';
 
 import {
   Bell,
+  ChevronDown,
   ChevronLeft,
   ChevronRight,
   Cloud,
@@ -17,26 +18,35 @@ import {
 
 export default function Navbar() {
   const [active, setActive] = useState(true);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
     <div
-      className={
-        active
-          ? 'flex flex-col min-w-[230px] min-h-screen p-2 bg-black'
-          : 'flex flex-col min-h-screen p-2 bg-black'
-      }
+      className={`flex flex-col min-h-screen p-2 bg-black overflow-hidden transition-[width] duration-300 ease-in-out ${
+        active ? 'w-[265px]' : 'w-[50px]'
+      }`}
     >
-      {active ? (
-        <>
-          <div className="flex flex-row mb-4">
-            <Image
-              src={games.at(9)!.portrait}
-              width={32}
-              height={32}
-              alt="profile"
-              className="w-[32px] h-[32px] rounded-full object-center cursor-not-allowed"
-            />
-            <div className="flex flex-row ml-auto gap-2">
+      <div className="flex flex-row items-center mb-4">
+        {active && (
+          <Image
+            src={games.at(9)!.portrait}
+            width={32}
+            height={32}
+            alt="profile"
+            className="w-[32px] h-[32px] rounded-full object-center cursor-not-allowed"
+          />
+        )}
+        <div className="flex flex-row ml-auto gap-2">
+          {active && (
+            <>
               <Home
                 height={32}
                 width={32}
@@ -49,76 +59,97 @@ export default function Navbar() {
                 color="white"
                 className="bg-gray-500/50 p-[4px] rounded-md cursor-not-allowed"
               />
-              <ChevronLeft
-                height={32}
-                width={32}
-                color="white"
-                onClick={() => setActive(!active)}
-                className="bg-gray-500/50 p-[4px] rounded-md cursor-pointer"
-              />
-            </div>
-          </div>
+            </>
+          )}
+          {active ? (
+            <ChevronLeft
+              height={32}
+              width={32}
+              color="white"
+              onClick={() => setActive(!active)}
+              className="bg-gray-500/50 p-[4px] rounded-md cursor-pointer"
+            />
+          ) : (
+            <ChevronRight
+              height={32}
+              width={32}
+              color="white"
+              onClick={() => setActive(!active)}
+              className="bg-gray-500/50 p-[4px] rounded-md cursor-pointer"
+            />
+          )}
+        </div>
+      </div>
+
+      {active && (
+        <>
           <div className="space-y-2 mb-2">
-            <button className="flex flex-row w-full items-center bg-gray-800/50 text-white p-2 rounded-md cursor-not-allowed">
-              <Home height={24} width={24} color="gray" />
-              <p className="ml-2">Home Page</p>
-            </button>
-            <button className="flex flex-row w-full items-center bg-gray-800/50 text-white p-2 rounded-md cursor-not-allowed">
-              <Library height={24} width={24} color="gray" />
-              <p className="ml-2">My Library</p>
-            </button>
-            <button className="flex flex-row w-full items-center bg-gray-800/50 text-white p-2 rounded-md cursor-not-allowed">
-              <Cloud height={24} width={24} color="gray" />
-              <p className="ml-2">Cloud Gaming</p>
-            </button>
-            <button className="flex flex-row w-full items-center bg-gray-800/50 text-white p-2 rounded-md cursor-not-allowed">
-              <Store height={24} width={24} color="gray" />
-              <p className="ml-2">Store</p>
-            </button>
+            {[
+              { Icon: Home, label: 'Home Page' },
+              { Icon: Library, label: 'My Library' },
+              { Icon: Cloud, label: 'Cloud Gaming' },
+              { Icon: Store, label: 'Store' },
+            ].map(({ Icon, label }) => (
+              <button
+                key={label}
+                className="flex flex-row w-full items-center bg-gray-800/50 text-white p-2 rounded-md cursor-not-allowed"
+              >
+                <Icon height={24} width={24} color="gray" />
+                <p className="ml-2 p-2 text-white/80">{label}</p>
+              </button>
+            ))}
           </div>
-          <div className="mb-2">
+
+          <div className="mb-2 relative">
             <select
-              className="my-2 block w-full p-2 rounded-lg bg-gray-800/50 cursor-not-allowed"
+              className="my-2 block w-full rounded-lg bg-gray-800/50 p-4 text-white/80 cursor-not-allowed appearance-none pr-10"
               value={1}
-              onChange={(e) => console.log(e.target.value)}
-              disabled={true}
+              disabled
             >
               <option value={1}>Action</option>
             </select>
+            <div className="absolute inset-y-0 right-4 flex items-center pointer-events-none">
+              <ChevronDown
+                height={24}
+                width={24}
+                color="white"
+                className="bg-gray-500/50 p-[4px] rounded-md cursor-not-allowed"
+              />
+            </div>
           </div>
+
           <div className="flex flex-col space-y-2">
-            <>
-              {games.slice(0, 3).map((item, i) => (
-                <div
-                  className="flex flex-row bg-gray-800/50 rounded-md p-2 items-center cursor-not-allowed"
-                  key={item.id}
-                >
-                  <Image
-                    src={item.portrait}
-                    width={48}
-                    height={48}
-                    alt="profile"
-                    className="min-w-[48px] h-[48px] rounded-md object-cover"
-                  />
-                  <div className="flex flex-col items-left w-full px-2">
-                    <h2 className="font-bold truncate">{item.name}</h2>
-                    <p className="text-sm text-gray-400 whitespace-nowrap">
-                      {`Last Played: ${i === 0 ? '0.5' : `${i}`} min ago`}
-                    </p>
+            {loading
+              ? Array(7)
+                  .fill(0)
+                  .map((_, i) => (
+                    <div
+                      className="flex flex-row w-[238.5px] h-[64px] bg-gray-800/50 rounded-md p-2 items-center cursor-not-allowed animate-[pulse_0.5s_infinite]"
+                      key={i}
+                    ></div>
+                  ))
+              : games.slice(0, 7).map((item, i) => (
+                  <div
+                    className="flex flex-row bg-gray-800/40 rounded-md p-2 items-center cursor-not-allowed"
+                    key={item.id}
+                  >
+                    <Image
+                      src={item.portrait}
+                      width={48}
+                      height={48}
+                      alt="profile"
+                      className="min-w-[48px] h-[48px] rounded-md object-cover brightness-80"
+                    />
+                    <div className="flex flex-col items-left w-full px-2">
+                      <h2 className="font-bold truncate">{item.name}</h2>
+                      <p className="text-sm text-gray-400 whitespace-nowrap">
+                        {`Last Played: ${i === 0 ? 'Now' : `${i} min ago`}`}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              ))}
-            </>
+                ))}
           </div>
         </>
-      ) : (
-        <ChevronRight
-          height={32}
-          width={32}
-          color="white"
-          onClick={() => setActive(!active)}
-          className="bg-gray-500/50 p-[4px] rounded-md cursor-pointer"
-        />
       )}
     </div>
   );
