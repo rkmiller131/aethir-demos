5c154da4a8bab9ee21d216e2db616e3b2a7a4bd2
rkmiller131 <rkmiller131@gmail.com>
2025-04-24 11:10:06 -0700

    updating to experimental config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['*.amazonaws.com'],
   images: {
     remotePatterns: [
       {
@@ -11,32 +12,37 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  async headers() {
-    return [
-      {
-        // Apply CORS headers to all routes
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Content-Type, Authorization',
-          },
-          {
-            key: 'Access-Control-Allow-Credentials',
-            value: 'true',
-          },
-        ],
-      },
-    ];
-  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*.amazonaws.com'],
+    },
+  }
+  // async headers() {
+  //   return [
+  //     {
+  //       // Apply CORS headers to all routes
+  //       source: '/:path*',
+  //       headers: [
+  //         {
+  //           key: 'Access-Control-Allow-Origin',
+  //           value: '*',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Methods',
+  //           value: 'GET, POST, PUT, DELETE, OPTIONS',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Headers',
+  //           value: 'X-Requested-With, Content-Type, Authorization',
+  //         },
+  //         {
+  //           key: 'Access-Control-Allow-Credentials',
+  //           value: 'true',
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
